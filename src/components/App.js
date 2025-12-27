fa2838dba72a44d34bb6350bfcee998787878262
= <=>
2025-12-27 22:46:05 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import "./../styles/App.css";
 
 const App = () => {
   const [bill, setBill] = useState(50);
-  const [tipPercentage, setTipPercentage] = useState(18);
+  const [tip, setTip] = useState(18);
   const [people, setPeople] = useState(1);
 
-  const totalTip = (bill * tipPercentage) / 100;
+  const totalTip = (bill * tip) / 100;
   const tipPerPerson =
     people > 0 ? (totalTip / people).toFixed(2) : "0.00";
 
@@ -14,36 +14,33 @@ const App = () => {
     <div id="container">
       {/* Do not remove the main div */}
 
-      <label>
-        Bill:
-        <input
-          type="number"
-          value={bill}
-          onChange={(e) => setBill(Number(e.target.value))}
-        />
-      </label>
+      <label htmlFor="billamt">Bill:</label>
+      <input
+        id="billamt"
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <br />
 
-      <label>
-        Tip Percentage:
-        <input
-          type="number"
-          value={tipPercentage}
-          onChange={(e) => setTipPercentage(Number(e.target.value))}
-        />
-      </label>
+      <label htmlFor="tipamt">Tip Percentage:</label>
+      <input
+        id="tipamt"
+        type="number"
+        value={tip}
+        onChange={(e) => setTip(Number(e.target.value))}
+      />
 
       <br />
 
-      <label>
-        Number of People:
-        <input
-          type="number"
-          value={people}
-          onChange={(e) => setPeople(Number(e.target.value))}
-        />
-      </label>
+      <label htmlFor="peopleamt">Number of People:</label>
+      <input
+        id="peopleamt"
+        type="number"
+        value={people}
+        onChange={(e) => setPeople(Number(e.target.value))}
+      />
 
       <p>Tip Per Person: ${tipPerPerson}</p>
     </div>
