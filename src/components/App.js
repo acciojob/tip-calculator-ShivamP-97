eaea46ee770b6c42b63ca3a21fbfc01e63252cac
= <=>
2025-12-27 23:03:08 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import "./../styles/App.css";
 
 const App = () => {
   const [bill, setBill] = useState(50);
-  const [tipPercentage, setTipPercentage] = useState(18);
+  const [tip, setTip] = useState(0.18);
   const [people, setPeople] = useState(1);
   const [tipPerPerson, setTipPerPerson] = useState("0.00");
 
   const calculateTip = () => {
-    const totalTip = (bill * tipPercentage) / 100;
+    const totalTip = bill * tip; 
     const result =
       people > 0 ? (totalTip / people).toFixed(2) : "0.00";
     setTipPerPerson(result);
@@ -32,8 +32,8 @@ const App = () => {
       <input
         id="serviceQual"
         type="number"
-        value={tipPercentage}
-        onChange={(e) => setTipPercentage(Number(e.target.value))}
+        value={tip}
+        onChange={(e) => setTip(Number(e.target.value))}
       />
 
       <br />
@@ -51,7 +51,7 @@ const App = () => {
       <button onClick={calculateTip}>Calculate</button>
 
       <div id="totalTip">
-        <p>Tip Per Person: ${tipPerPerson}</p>
+        ${tipPerPerson} each
       </div>
     </div>
   );
